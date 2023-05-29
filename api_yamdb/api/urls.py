03f4486e3fc162abf7cbda6c0802323b71042085
Artemis1359 <tmnt99@yandex.ru>
2023-05-29 20:23:36 +0300

    Commit 5

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import UserViewSet
+from .views import signup, token, UserViewSet
 
 router_v1 = routers.DefaultRouter()
 router_v1.register(r'users', UserViewSet, basename='users')
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
+    path('v1/auth/signup/', signup, name='signup'),
+    path('v1/auth/token/', token, name='token'),
 ]
